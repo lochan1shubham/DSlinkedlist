54974e22fbbd67de2e07961a4ec547c90182b364
shubhamlochan <shubham1lochan@gmail.com>
2025-07-23 10:10:07 +0530

    Added sliding window MAx sum

---
+++ src/test/QueueTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class QueueTest {
+	
+	int front = 0;
+	int rear = -1;
+	final int MAX = 9;
+	int [] queue = new int[MAX];
+	
+	public static void main(String[] args) {
+		System.out.println("Queue Test");
+		
+	}
+}
